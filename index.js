d4912a68246deb4ec3ab9ebc0253105e9027c304
merndevreza <merndevreza@gmail.com>
2023-11-08 23:15:11 +0600

    JWT token authentication added for all books and add book route

--- index.js
+++ index.js
@@ -6,14 +6,39 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //======================
 //middleware
 //======================
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://knowledge-bridge-d084c.web.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser())
+// My Middlewares
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("Value of token in Middleware", token);
+  if (!token) {
+    return res.status(401).send({ message: "Not Authorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // Error
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    // If the user is valid then it would be decoded
+    req.user = decoded;
+    next();
+  });
+};
 //======================
 //MongoDB config
 //======================
@@ -33,21 +58,44 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //=================
+    // Authentication
+    //=================
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body;
+      const token = jwt.sign(
+        userEmail,
+        "7429aef8c1fd9ed474e980cd8b5e9760aef2214928cd3fd896064df38efdedb2163bf796c22cba3fdcaac6188b18838bbaef1b56b6be2aa9bc68ec770be710c4",
+        {
+          expiresIn: "1h",
+        }
+      );
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+        })
+        .send({ success: true });
+    });
+    app.post("/log-out", async (req, res) => {
+      res.clearCookie("token").send({ success: true });
+    });
+
+    //==========
+    // Books
+    //==========
 
     const database = client.db("knowledgeBridge");
     const booksCollection = database.collection("books");
     const BorrowedBooksCollection = database.collection("BorrowedBooks");
-    //==========
-    // Books
-    //==========
     // add book
-    app.post("/books", async (req, res) => {
+    app.post("/books",verifyToken, async (req, res) => {
       const book = req.body;
       const result = await booksCollection.insertOne(book);
       res.send(result);
     });
     //read all book
-    app.get("/books", async (req, res) => {
+    app.get("/books",verifyToken, async (req, res) => {
       const result = await booksCollection.find().toArray();
       res.send(result);
     });
@@ -70,13 +118,13 @@ async function run() {
           photo: updateRequest.photo,
           category: updateRequest.category,
           rating: updateRequest.rating,
-          quantity: updateRequest.quantity
+          quantity: updateRequest.quantity,
         },
       };
       const result = await booksCollection.updateOne(query, updateDoc);
       res.send(result);
     });
-    
+
     // read category products
     app.get("/category/:id", async (req, res) => {
       const categoryName = req.params.id;
@@ -87,22 +135,23 @@ async function run() {
     //==========
     // Borrowed Books
     //==========
-    app.post("/borrowed-books",async(req,res)=>{
-      const borrowedBook= req.body;
-      const result= await BorrowedBooksCollection.insertOne(borrowedBook);
-      res.send(result)
-    })
-    app.get("/borrowed-books/:id",async(req,res)=>{
+    app.post("/borrowed-books", async (req, res) => {
+      const borrowedBook = req.body;
+      const result = await BorrowedBooksCollection.insertOne(borrowedBook);
+      res.send(result);
+    });
+    app.get("/borrowed-books/:id", async (req, res) => {
       const userEmail = req.params.id;
       const query = { userEmail: userEmail };
       const result = await BorrowedBooksCollection.find(query).toArray();
       res.send(result);
-    })
-
-
-
-
-
+    });
+    app.delete("/borrowed-books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await BorrowedBooksCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
